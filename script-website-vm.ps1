0beebb35d5ad70948f7fca56912f5d0315c3bea2
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2019-10-10 02:13:22 +0530

    Update script-website-vm.ps1

--- script-website-vm.ps1
+++ script-website-vm.ps1
@@ -7,30 +7,9 @@ Import-Module ServerManager
 install-windowsfeature web-server, web-webserver -IncludeAllSubFeature
 install-windowsfeature web-mgmt-tools
 
-# Download the files for our web application.
-Set-Location -Path C:\inetpub\wwwroot
+#Change physical path of a site
+Import-Module WebAdministration
+Set-ItemProperty -path "IIS:\Sites\Default Web Site\" -name physicalPath -value "f:\website-code-300-01-01"
 
-$shell_app = new-object -com shell.application
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/ahmadzahoory/az300template/master/vm-website-01.zip", (Get-Location).Path + "\azure-vm-website.zip")
-
-$zipfile = $shell_app.Namespace((Get-Location).Path + "\azure-vm-website.zip")
-$destination = $shell_app.Namespace((Get-Location).Path)
-$destination.copyHere($zipfile.items())
-
-# Create the web app in IIS
-New-WebApplication -Name netapp -PhysicalPath c:\inetpub\wwwroot\vm-website -Site "Default Web Site" -force
-
-# The following code will create an 2nd IIS site and it associated Application Pool. 
-
-$SiteFolderPath = "F:\dashboard"          # Website Folder
-$SiteAppPool = "my2ndwebserver"             # Application Pool Name
-$SiteName = "my2ndwebserver"                # IIS Site Name
-
-New-Item $SiteFolderPath -type Directory
-New-Item IIS:\AppPools\$SiteAppPool
-New-Item IIS:\Sites\$SiteName -physicalPath $SiteFolderPath -bindings @{protocol="http";bindingInformation=":8080:"}
-Set-ItemProperty IIS:\Sites\$SiteName -name applicationPool -value $SiteAppPool
-
-
-# Open Port 8080. 
-New-NetFirewallRule -DisplayName "Allow Inbound Port 8080" -Direction inbound -LocalPort 8080 -Protocol TCP -Action Allow
+#Restart IIS
+iisreset /restart
