a9d32b4d87c1fec1adb2e4bfc1e25213d12ee867
ahmadzahoory <33231461+ahmadzahoory@users.noreply.github.com>
2020-01-26 23:01:36 +0530

    Update az-300-03-script-b.ps1

--- az-300-03-script-b.ps1
+++ az-300-03-script-b.ps1
@@ -19,3 +19,6 @@ $destination.copyHere($zipfile.items())
 
 # Create the web app in IIS
 New-WebApplication -Name netapp -PhysicalPath c:\inetpub\wwwroot\vnet-app-website -Site "Default Web Site" -force
+
+#Open ICMPv4 Firewall Rule
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" dir=in action=allow enable=yes protocol=icmpv4:8,any
